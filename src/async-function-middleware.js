675285b91e41214b25826402bce808f93bae6399
Joel Hernandez <involvmnt@gmail.com>
2016-12-11 18:17:43 +0100

    feat(async-function-middleware): blueprint

---
+++ src/async-function-middleware.js
@@ -0,0 +1,9 @@
+/**
+ * Created on 11/12/16.
+ * @author Joel Hernandez <joel.hernandez@kpn.com>
+ */
+import FunctionMiddleware from './function-middleware';
+
+export default class AsyncFunctionMiddleware extends FunctionMiddleware {
+
+}
